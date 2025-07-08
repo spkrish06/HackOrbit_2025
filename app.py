6a91df71ff3c4b547fd99bd14366b208518f2521
spkrish06 <sp.krish06@gmail.com>
2025-07-08 12:39:50 +0300

    Updated index.html to display VWAP strategy output and added structural layout for AI chatbot (logic not implemented yet). Started setup for second trading strategy.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for, flash
+from vwap_strategy import run_strategy_vwap, get_trade_df
+import plotly.io as pio
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
@@ -17,9 +20,66 @@ def login():
     return render_template('login.html') 
 
 
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    metrics = None
+    plot_div = None
+    selected_strategies = []
+
+    if request.method == 'POST':
+        strategy = request.form['strategy']
+        stock = request.form['stock']
+        invest_cap = float(request.form['invest_cap'])
+        turnover = int(request.form['turnover'])
+        min_trade_bal = float(request.form['min_trade_bal'])
+        is_crypto = request.form.get('is_crypto', 'false').lower() == 'true'
+        rf_symbol = request.form.get('rf_symbol', '^IRX')  
+
+       
+        if min_trade_bal >= invest_cap:
+            flash("Minimum Trade Balance must be less than Investment Capital.", "warning")
+            return redirect(url_for('index'))
+
+        session.update({
+            'stock': stock,
+            'invest_cap': invest_cap,
+            'turnover': turnover,
+            'min_trade_bal': min_trade_bal
+        })
+
+        if strategy == 'vwap':
+            results = run_strategy_vwap(stock, invest_cap, turnover, min_trade_bal, is_crypto, rf_symbol)
+        else:
+            flash("More strategies coming soon!!")
+            return redirect(url_for('index'))
+       
+        plot_div = pio.to_html(results['plotly_fig'], full_html=False)
+        metrics = {
+                'invested_capital': results['Invested Capital'],
+                'portfolio_value': results['Portfolio value'],
+                'sharpe_ratio': results['sharpe'],
+                'sortino_ratio': results['sortino'],
+                'calmar_ratio': results['calmar'],
+                'std': results['std'],
+                'mean': results['mean'],
+                'median': results['median'],
+                'win_rate': results['win_rate'],
+                'loss_rate': results['loss_rate'],
+                'max_drawdown': results['max_drawdown'],
+                'cagr': results['CAGR']
+                }
+    
+        session['latest_metrics'] = metrics
+        session['portfolio_data'] = results.get('portfolio_data')
+        session['latest_csv'] = results['csv_path']
+        selected_strategies = [strategy]
+
+    return render_template(
+        'index.html',
+        metrics=metrics,
+        plot_div=plot_div,
+        selected_strategies=selected_strategies
+    )
 
 
 @app.route('/trades')
